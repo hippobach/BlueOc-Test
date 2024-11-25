94ce4b3edea7ee9bd043f60018996b659f8d86c4
Bach Nguyen <nguyenxuanbach03nd@gmail.com>
2024-11-25 20:50:01 +0700

    optimize code for task 2

--- Task_2/index.js
+++ Task_2/index.js
@@ -1,19 +1,22 @@
-// Sum Of Top Two Integer
 const sumOfTopTwoInteger = (arr) => {
-  // Tìm giá trị lớn nhất
-  const max = Math.max(...arr);
-  // Loại bỏ tất cả các giá trị lớn nhất (chỉ giữ lại một bản)
-  arr = arr.filter((num) => num !== max);
-  // Tìm giá trị lớn nhất thứ hai
-  const sub_max = Math.max(...arr);
-  return max + sub_max;
+  if (arr.length < 2) {
+    throw new Error('Array must have at least two elements');
+  }
+  // Sử dụng Set trong ES6 để lưu trữ tập hợp các giá trị duy nhất
+  const uniqueArray = Array.from(new Set(arr));
+  uniqueArray.sort((a, b) => b - a);
+
+  if (uniqueArray.length < 2) {
+    throw new Error('Array must have at least two unique elements');
+  }
+  return uniqueArray[0] + uniqueArray[1];
 };
 
 const array1 = [1, 4, 2, 3, 5];
-console.log(sumOfTopTwoInteger(array1));
+console.log(sumOfTopTwoInteger(array1)); // Output: 9
 
 const array2 = [1, 4, 2, 5, 3, 5];
-console.log(sumOfTopTwoInteger(array2));
+console.log(sumOfTopTwoInteger(array2)); // Output: 9
 
 const array3 = [12, 7, 2, 3, 5, 1, 6, 4, 12, 7, 7, 12];
-console.log(sumOfTopTwoInteger(array3));
+console.log(sumOfTopTwoInteger(array3)); // Output: 19
